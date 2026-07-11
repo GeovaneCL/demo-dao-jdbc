bc12fcd6388167044a01c8e7757969a2965689c8
Geovane Caixeta <caixetageovane@gmail.com>
2026-07-10 23:06:57 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.FactoryDao;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 import java.util.Date;
@@ -11,6 +13,8 @@ public class Program {
 		Department obj = new Department(6, "Music");
 		
 		Seller seller = new Seller(120,"Victor","victor@gmail.com",new Date(),1621.00,obj);
+		
+		SellerDao sellerDao = FactoryDao.createSellerDao();
 		
 		System.out.println(seller);
 	}
