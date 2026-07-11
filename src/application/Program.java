ce750197072581cb7e6df53cc25ec665f062b713
Geovane Caixeta <caixetageovane@gmail.com>
2026-07-10 22:06:09 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+import java.util.Date;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(6, "Music");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(120,"Victor","victor@gmail.com",new Date(),1621.00,obj);
+		
+		System.out.println(seller);
 	}
 }
