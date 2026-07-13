a73a90a06948b681c3b2ea740a4d91e6063f32bd
Geovane Caixeta <caixetageovane@gmail.com>
2026-07-13 19:19:28 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,13 +15,19 @@ public class Program {
 		
 		System.out.println("----TEST 1: seller findById----");
 		Seller seller = sellerDao.findById(2);
-		
 		System.out.println(seller);
+		
 		System.out.println("\n----TEST 2: seller findByDepartment----");
 		Department department = new Department(2,null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for(Seller obj : list) {
 			System.out.println(obj);
+		}
+		
+		System.out.println("\n----TEST 3: seller findByAll----");
+		list = sellerDao.findAll();
+		for(Seller obj : list) {
+		System.out.println(obj);
 		}
 	}
 }
