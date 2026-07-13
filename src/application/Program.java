5e36b272836187103931ec9e90749eccff5296c2
Geovane Caixeta <caixetageovane@gmail.com>
2026-07-13 20:03:38 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.FactoryDao;
@@ -29,5 +30,11 @@ public class Program {
 		for(Seller obj : list) {
 		System.out.println(obj);
 		}
+		
+		System.out.println("\n----TEST 4: seller insert----");
+		Seller newseller = new Seller(null,"Mark","Mark@gmail.com", new Date(), 4000.00,department);
+		sellerDao.insert(newseller);
+		
+		System.out.println("Inserted! New id = "+ newseller.getId());
 	}
 }
