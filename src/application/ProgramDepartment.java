72c6079a55b36cef57fdafc3ec210c5df8974fcc
Geovane Caixeta <caixetageovane@gmail.com>
2026-07-14 20:15:27 -0300

    DepartmentDaoJDBC findById implementation

--- src/application/ProgramDepartment.java
+++ src/application/ProgramDepartment.java
@@ -2,15 +2,13 @@ package application;
 
 import model.dao.DepartmentDao;
 import model.dao.FactoryDao;
-
 import model.entities.Department;
 
 public class ProgramDepartment {
 	public static void main(String[]args) {
-	Department department = new Department(8,"Ifood");
 	
 	DepartmentDao departmentDao = FactoryDao.createDepartmentDao();
-	
+	Department department = departmentDao.findById(2);
 	System.out.println(department);
 	}
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,13 +1,24 @@
 package model.dao.impl;
 
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentDaoJDBC implements DepartmentDao{
 	
+	private Connection conn;
+	
+	public DepartmentDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
 	@Override
 	public void insert(Department obj) {
 		// TODO Auto-generated method stub
@@ -24,12 +35,34 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
 		
+		
 	}
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st =null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+				  "SELECT id, Name as DepName "
+				 +"FROM department "
+				 +"WHERE id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if(rs.next()) {
+				Department dp = new Department();
+				dp.setId(rs.getInt("Id"));
+				dp.setName(rs.getString("DepName"));
+				return dp;
+			}
+			return null;
+			
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally{
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
